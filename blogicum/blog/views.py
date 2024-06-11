aa8a23cdedb31993218e2245fca8a267a814533c
NailMansurov <mansurov-nf@live.com>
2024-06-12 00:49:29 +0500

    The 1st review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
